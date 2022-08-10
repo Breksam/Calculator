3e55dcb71c009c9001848199f4966f1fbfd9e794
Breksam Elsokkary <76849788+Breksam@users.noreply.github.com>
2022-08-10 23:13:34 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// determine the equation and get result
+function result() {
+    field.value= eval(field.value);
+}
+
+// delete one by one from input value
+function deleteNum(){
+    let num = document.getElementById("field").value;
+    let deletenum= num.substring(0, num.length - 1);
+    document.getElementById("field").value = deletenum; 
+}
